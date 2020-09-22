2ffed1d36985a6bc7f3075da8a9dc01e5f1ed063
Faiz Ahmad <55245031+faiz11amd@users.noreply.github.com>
2020-09-22 21:47:10 +0530

    Updates

---
+++ precache-manifest.07944e2068cc308f296806c04924bcfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98a6c0b30420e376513506ca700c87f",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "791f52e4222ec4786049",
+    "url": "/deploy-react-app/static/css/main.09cf0cdc.chunk.css"
+  },
+  {
+    "revision": "dcd74c206acb1f66eb4b",
+    "url": "/deploy-react-app/static/js/2.2750b63e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-react-app/static/js/2.2750b63e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791f52e4222ec4786049",
+    "url": "/deploy-react-app/static/js/main.569d3075.chunk.js"
+  },
+  {
+    "revision": "04ac5ed44b1b47c8ab7c",
+    "url": "/deploy-react-app/static/js/runtime-main.919e31d5.js"
+  }
+]);
